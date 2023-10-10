a28fb6a9f3df54fcad5d749565de5642d17967ae
“shawazi” <shawazisonfire@gmail.com>
2023-10-10 17:06:15 -0400

    html form prefab

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+
 
 const Authentication = () => {
   return (
-    <Box>
-    </Box>
+    <div>
+      auth
+    </div>
   );
 };
 
